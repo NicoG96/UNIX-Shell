dd5832d79b4b89ada918e960aaccabbf14c085b2
Nico Gonzalez <nicog96@gmail.com>
2018-10-11 19:37:05 -0400

    Set up logical conditions to accommodate batch file execution.

--- main.c
+++ main.c
@@ -1,7 +1,24 @@
 #include "shell.h"
 
-int main() {
-    run_shell();
+int main(int argc, char **argv) {
+    if (argc > 2) {
+        fprintf(stderr, "Too many arguments.\n");
+        return 1;
 
+    //batch command
+    }else if (argc == 2) {
+        FILE* batch;
+        batch = fopen(argv[1], "r");
+
+        if (!batch) {
+            fprintf(stderr, "Empty batch file.");
+        } else{
+            //run batch commands
+        }
+        fclose(batch);
+
+    }else{
+        run_shell();
+    }
     return 0;
 }
