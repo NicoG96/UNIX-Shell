162d587b8f24e3c563b3d963b557ef4bd12dc0fc
Nico Gonzalez <nicog96@gmail.com>
2018-10-12 04:47:01 -0400

    Change how SHELL_PATH is stored, add structures for redirections, piping, and backgrounding.

--- shell.c
+++ shell.c
@@ -4,20 +4,19 @@
 #pragma clang diagnostic ignored "-Wmissing-noreturn"
 
 void run_shell() {
-    //environ vars
-    extern char *environment[];
-
-    //get user & cwd
-    struct passwd *CURR_USR = getpwuid(getuid());
-    char CURR_PATH[1024];
+    //set shell path in the environment
+    char pwd[BUFFER];
+    getcwd(pwd, sizeof(pwd));
+    strcat(pwd, "/myshell");
+    setenv("SHELL_PATH", pwd, 1);
 
     //var for command line
     char line[BUFFER];
 
-    //array to hold tokens
+    //array to hold arg tokens
     char *argv[BUFFER];
 
-    //counter for arguments
+    //counter for # of arguments
     int argc = 0;
 
     while(1) {
@@ -25,7 +24,7 @@ void run_shell() {
         fflush(stdin);
 
         //prompt
-        printf("%s@%s%s ", getcwd(CURR_PATH, sizeof(CURR_PATH)), CURR_USR->pw_name, PROMPT);
+        printf("%s@%s%s ", getcwd(pwd, sizeof(pwd)), getenv("USER"), PROMPT);
 
         //get user input
         fgets(line, BUFFER, stdin);
@@ -36,84 +35,147 @@ void run_shell() {
             line[len - 1] = '\0';
         }
 
-        //parse it
-        parse(line, &argc, argv);
+        //variable to hold state, assume normal input
+        int state = normal;
 
-        //attempt to run command
-        run_cmd(argc, argv);
+        //parse it
+        parse(line, &argc, argv, &state);
+
+        //attempt to run as internal command
+        run_shell_cmd(argc, argv, &state);
     }
 }
 #pragma clang diagnostic pop
 
-void run_cmd(int argc, char **argv) {
-    /* INTERNALS */
+void run_shell_cmd(int argc, char **argv, int* state) {
     if (strcmp(argv[0], "environ") == 0) {
         environ();
+        //output redir
     } else if(strcmp(argv[0], "echo") == 0) {
         echo(argv);
+        //
+    }else if (strcmp(argv[0], "help") == 0) {
+        help();
+        //
+    }else if(strcmp(argv[0], "dir") == 0) {
+        dir(argc, argv);
+        //
     }else if(strcmp(argv[0], "cd") == 0) {
         cd(argc, argv);
     }else if(strcmp(argv[0], "clear") == 0) {
         clear();
-    }else if(strcmp(argv[0], "dir") == 0) {
-        dir(argc, argv);
     }else if(strcmp(argv[0], "pause") == 0) {
         pause_cmd();
     }else if(strcmp(argv[0], "quit") == 0) {
         quit_cmd();
-    }else if (strcmp(argv[0], "help") == 0) {
-        help();
 
-
-    /* EXTERNALS */
+    //attempt to run as external cmd
     }else {
-        ext_exe(argc, argv);
+        run_ext_exe(argc, argv, state);
     }
 }
 
-void ext_exe(int argc, char **argv) {
-    //get cwd
-    char CURR_PATH[1024];
-    getcwd(CURR_PATH, sizeof(CURR_PATH));
-
-    //begin fork
-    int rc = fork();
-
-    if (rc < 0) {
-        perror("Fork failure.");
-        exit(EXIT_FAILURE);
+void run_ext_exe(int argc, char **argv, int* state) {
+    if (*state == normal) {
+        //fork process
+        pid_t rc = fork();
+        if (rc < 0) {
+            perror("Fork Failure.\n");
+            exit(EXIT_FAILURE);
+        }
+        //parent
+        else if (rc > 0) {
+            int status = 0;
+            wait(&status);
+        }
+        //child
+        else {
+            execvp(argv[0], argv);
+            perror("Child fork problem.");
+            exit(EXIT_FAILURE);
+        }
     }
-    //parent
-    if (rc > 0) {
-        wait(NULL);
-        //unless backgrounded
 
-    //child
-    } else{
-        execvp(argv[0], argv);
-        /*
-         * 1. look in curr dir
-         * 2. look at specified dir
-         * 3. look at environ dir
-         * 4. fork should b executed w parent path
-         * test externals:
-         *  ls -la | wc -l
-         *  ls -la >> output.txt
-         *  sort < input.txt
-         */
+    else if (*state == red_in) {
+        //do stuff
+    }
 
+    else if (*state == red_out) {
+        //yep
+    }
 
-        /*
-         * BG EXEC
-         * execute(char** argv){
-            if((pid = fork()) == 0){
-            is redirection?
-            is piping?
-            exec(argv[0],argv);
+    else if (*state == dub_red_in) {
+        //test
+    }
+
+    else if (*state == dub_red_out) {
+        //hello
+        int i = 0;
+    }
+
+    else if (*state == piping){
+        //piping fx
+        int o = 4;
+    }
+
+    else {
+        //backgrounding, disable stdout
+    }
+}
+
+void redirects() {
+    int fd;
+    //int fd = fopen("output", O_CREAT|O_WRONLY, S_IRWXU);
+    printf("%d\n", fd);
+    dup2(fd, 1); //Change standard output(1) to be the value of fd.
+    printf("This text is not on the terminal, but in the output file.\n");
+    close(fd);
+}
+
+void pipes() {
+    /*
+     * shellcmdhandler handler1 = findhandler(cmdlist, argv1[0])
+     * shellcmdhandler handler2 = findhandler(cmdlist, argv2[0])
+     */
+
+    //create file descriptor
+    int pfds[2];
+
+    //create pipe b4 fork, pipe shared by both processes
+    if (pipe(pfds) == 0) {
+        //child
+        if (fork() == 0) {
+            //close stdout
+            close(1);
+
+            //set stdout at write end
+            dup2(pfds[1], 1);
+
+            //close read end
+            close(pfds[0]);
+
+            /*
+            if (handler1 != NULL) {
+                handler1(argv1[0], argv1);
             }
-            else if(is’&’<0) { // "&" means no wait for parent process,
-                waitpid(pid,&status,0);
+             */
+        }
+        //parent
+        else {
+            //close stdin
+            close(0);
+
+            //set stdin as read end
+            dup2(pfds[0], 0);
+
+            //close write end
+            close(pfds[1]);
+
+            /*
+            if (handler2 != NULL) {
+                handler2(argv2[0], argv2);
             }
-         */
+             */
+        }
     }
 }
