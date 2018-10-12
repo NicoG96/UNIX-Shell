9e31247e77386278be660ad505ff4c90fdbb1a10
Nico Gonzalez <nicog96@gmail.com>
2018-10-11 20:49:43 -0400

    Add enums for redirections, piping, backgrounding.

--- shell.h
+++ shell.h
@@ -12,6 +12,8 @@
 #include <errno.h>
 #include <pwd.h>
 #include <sys/utsname.h>
+extern int errno;
+extern char *environment[];
 
 //macros
 #define BUFFER 128
@@ -19,15 +21,28 @@
 
 //functions
 void run_shell();
-void parse(char* line, int* argc, char* argv[]);
+void parse(char* line, int* argc, char* argv[], int* state);
 void echo(char **argv);
-void environment();
+void environ();
 void cd(int argc, char **argv);
 void clear();
 void dir(int argc, char **argv);
 void help();
 void pause_cmd();
 void quit_cmd();
+void run_cmd(int argc, char** argv, int* state);
+void ext_exe(int argc, char** argv, int* state);
 void sandbox();
+
+//enum
+enum state {
+    normal,
+    red_in,
+    red_out,
+    dub_red_in,
+    dub_red_out,
+    piping,
+    background
+};
 
 #endif
