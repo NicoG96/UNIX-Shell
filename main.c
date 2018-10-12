e01d23be3b9986622010a28cde74c4cb26f2663f
Nico Gonzalez <nicog96@gmail.com>
2018-10-12 04:49:44 -0400

    Restructure code.

--- main.c
+++ main.c
@@ -1,22 +1,19 @@
 #include "shell.h"
 
 int main(int argc, char **argv) {
-    if (argc > 2) {
-        fprintf(stderr, "Too many arguments.\n");
-        return 1;
-
-    //batch command
-    }else if (argc == 2) {
+    //if batch file
+    if (argc >= 2) {
         FILE* batch;
         batch = fopen(argv[1], "r");
 
         if (!batch) {
-            fprintf(stderr, "Empty batch file.");
+            fprintf(stderr, "Empty batch file.\n");
         } else{
             //run batch commands
+            fclose(batch);
         }
-        fclose(batch);
 
+    //get user input otherwise
     }else{
         run_shell();
     }
