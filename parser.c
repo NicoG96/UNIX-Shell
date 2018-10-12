fbbb7b089b4c01c419f9adb2c5ab740c1aed209c
Nico Gonzalez <nicog96@gmail.com>
2018-10-11 20:49:07 -0400

    Update parser to check for redirections, piping, and backgrounding.

--- parser.c
+++ parser.c
@@ -1,9 +1,23 @@
 #include "shell.h"
 
-void parse(char* line, int* argc, char* argv[]) {
+void parse(char* line, int* argc, char* argv[], int* state) {
     char *token = strtok(line, " ");
     int i = 0;
     while(token != NULL) {
+        //check for redirection, piping, back-grounding
+        if (strcmp(token, "<") == 0) {
+            *state = red_in;
+        } else if (strcmp(token, ">") == 0) {
+            *state = red_out;
+        } else if (strcmp(token, "<<") == 0) {
+            *state = dub_red_in;
+        }else if ((strcmp(token, ">>") == 0)) {
+            *state = dub_red_out;
+        }else if ((strcmp(token, "|") == 0)) {
+            *state = piping;
+        }else if ((strcmp(token, "&") == 0)) {
+            *state = background;
+        }
         argv[i++] = token;
         token = strtok(NULL, " ");
     }
