8d3b3f1b08d58e9e022a65d2e55986f3906c69a6
Nico Gonzalez <nicog96@gmail.com>
2018-10-16 18:23:11 -0400

    Make code more readable, add special logic for backgrounding.

--- parser.c
+++ parser.c
@@ -1,22 +1,31 @@
 #include "shell.h"
 
-void parse(char* line, int* argc, char* argv[], int* state) {
+void parse(char* line, int* argc, char** argv, int* state) {
     char *token = strtok(line, " ");
     int i = 0;
+
     while(token != NULL) {
-        //check for redirection, piping, back-grounding
+        //check for redirection, piping, backgrounding
         if (strcmp(token, "<") == 0) {
             *state = red_in;
-        } else if (strcmp(token, ">") == 0) {
-            *state = red_out;
-        } else if (strcmp(token, "<<") == 0) {
+        }
+        else if (strcmp(token, ">") == 0) {
+            *state == red_in ? (*state = in_out) : (*state = red_out);
+        }
+        else if (strcmp(token, "<<") == 0) {
             *state = dub_red_in;
-        }else if ((strcmp(token, ">>") == 0)) {
+        }
+        else if ((strcmp(token, ">>") == 0)) {
             *state = dub_red_out;
-        }else if ((strcmp(token, "|") == 0)) {
+        }
+        else if ((strcmp(token, "|") == 0)) {
             *state = piping;
-        }else if ((strcmp(token, "&") == 0)) {
+        }
+        else if ((strcmp(token, "&") == 0)) {
             *state = background;
+
+            //if it's an '&' then it's the end of command and can break out of loop
+            break;
         }
         argv[i++] = token;
         token = strtok(NULL, " ");
