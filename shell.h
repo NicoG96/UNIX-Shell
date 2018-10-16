2de7e3c90dc3696b9695dd920106a50a252f2208
Nico Gonzalez <nicog96@gmail.com>
2018-10-16 18:22:21 -0400

    Update functions for redirects/pipes.
    Add enum for input with output redirection.
    
    Add separate function for shell, backgrounding.

--- shell.h
+++ shell.h
@@ -20,7 +20,8 @@
 
 //functions
 void run_shell();
-void parse(char* line, int* argc, char* argv[], int* state);
+void run_batch(int argc, char** argv);
+void parse(char* line, int* argc, char** argv, int* state);
 void echo(char **argv);
 void environ();
 void cd(int argc, char **argv);
@@ -29,10 +30,11 @@ void dir(int argc, char **argv);
 void help();
 void pause_cmd();
 void quit_cmd();
-void run_shell_cmd(int argc, char** argv, int* state);
-void run_ext_exe(int argc, char** argv, int* state);
-void pipes();
-void redirects();
+int run_shell_cmd(int argc, char** argv, int state);
+void run_ext_exe(int argc, char** argv, int state);
+void pipes(char** argv);
+void redirects(char **argv, int state);
+void backgrounding(char** argv);
 void sandbox();
 
 //enum
@@ -42,6 +44,7 @@ enum state {
     red_out,
     dub_red_in,
     dub_red_out,
+    in_out,
     piping,
     background
 };
