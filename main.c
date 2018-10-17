734762b34e19aaa70d95314fe3f01faae12f7a35
Nico Gonzalez <nicog96@gmail.com>
2018-10-16 22:05:32 -0400

    Add test code.

--- main.c
+++ main.c
@@ -9,11 +9,13 @@ int main(int argn, char **args) {
     */
     //if there's arguments, run as batch file
     if (argn > 1) {
+        //puts("Executing batch ...");
         batch_exe(argn, args);
     }
 
     //otherwise, get command from user
     else {
+        //puts("Running shell normally ..");
         run_shell();
     }
     return 0;
