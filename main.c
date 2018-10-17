fb269466052d73ca5db4484d1cf2c6eb63e8c8cb
Nico Gonzalez <nicog96@gmail.com>
2018-10-17 08:20:26 -0400

    More test code.

--- main.c
+++ main.c
@@ -3,11 +3,14 @@
 int main(int argc, char **argv) {
     /*
     //print arguments
+    printf("argc: %d\n", argc);
     for (int i = 0; i < argc; i++) {
-        printf("%s\n", argv[i]);
+        printf("argv[%d]:\t%s\n", i, argv[i]);
     }
-    */
+     */
+
     //sandbox();
+
     run_shell(argc, argv);
     return 0;
 }
