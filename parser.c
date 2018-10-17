1d4749da3f7e74e50405108d82970da4a348943b
Nico Gonzalez <nicog96@gmail.com>
2018-10-17 03:16:52 -0400

    Add functionality that separates command line (if redirection/piping) into 2 arrays.

--- parser.c
+++ parser.c
@@ -1,39 +1,62 @@
 #include "shell.h"
 
-void parse(char* line, int* argc, char** argv, int* state) {
+int parse(char* line, int* argc1, char** argv1, char** argv2) {
     char *token = strtok(line, " ");
-    int i = 0;
+    int state = normal;
+    int argc2 = 0;
+    char **argv = argv1;
+    int *argc = argc1;
 
     while(token != NULL) {
         //check for redirection, piping, backgrounding
         if (strcmp(token, "<") == 0) {
-            *state = red_in;
+            state = red_in;
+            argv = argv2;
+            argc = &argc2;
+            token = strtok(NULL, " ");
         }
         else if (strcmp(token, ">") == 0) {
-            *state == red_in ? (*state = in_out) : (*state = red_out);
-        }
-        else if (strcmp(token, "<<") == 0) {
-            *state = dub_red_in;
+            //if there was an input redirection earlier
+            if (state == red_in) {
+                //it's a special type
+                state = in_out;
+                token = strtok(NULL, " ");
+
+                //have argv2 hold the 2 redirections
+                argv[*argc] = calloc(1, strlen(token));
+                strcpy(argv[*argc], token);
+                (*argc)++;
+                argv[*argc] = NULL;
+                return state;
+            }
+            state = red_out;
+            argv = argv2;
+            argc = &argc2;
+            token = strtok(NULL, " ");
         }
         else if ((strcmp(token, ">>") == 0)) {
-            *state = dub_red_out;
+            state = dub_red_out;
+            argv = argv2;
+            argc = &argc2;
+            token = strtok(NULL, " ");
         }
         else if ((strcmp(token, "|") == 0)) {
-            *state = piping;
+            state = piping;
+            argv = argv2;
+            argc = &argc2;
+            token = strtok(NULL, " ");
         }
         else if ((strcmp(token, "&") == 0)) {
-            *state = background;
+            state = background;
 
-            //if it's an '&' then it's the end of command and can break out of loop
-            break;
+            //& always end of command, so we can exit
+            return state;
         }
-        argv[i++] = token;
+        argv[*argc] = calloc(1, strlen(token));
+        strcpy(argv[*argc], token);
+        (*argc)++;
+        argv[*argc] = NULL;
         token = strtok(NULL, " ");
     }
-
-    //append NULL to signify end of cmd
-    argv[i] = NULL;
-
-    //record arg # into argc
-    *argc = i;
+    return state;
 }
